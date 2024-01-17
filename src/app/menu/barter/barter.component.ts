e1836e7464f4cad92add513df919d369b0886ff9
Félix DAUNE <felix.daune@ecole.ensicaen.fr>
2024-01-17 16:37:26 +0100

    Basic HTML page

--- src/app/menu/barter/barter.component.ts
+++ src/app/menu/barter/barter.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BarterService } from './barter.service';
 
 @Component({
   selector: 'app-barter',
   templateUrl: './barter.component.html',
   styleUrls: ['./barter.component.scss']
 })
-export class BarterComponent {
 
+export class BarterComponent implements OnInit {
+
+  constructor(private service:BarterService) {
+
+  }
+
+  ngOnInit(): void {
+  }
+  
 }
