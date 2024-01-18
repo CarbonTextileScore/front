a343fa443326009172cc8ded61239fd6a469660f
BlueC0C0 <60580265+BlueC0C0@users.noreply.github.com>
2024-01-18 20:25:39 +0100

    je sais plus

--- src/app/domain/user.dto.ts
+++ src/app/domain/user.dto.ts
@@ -1,83 +1,15 @@
 export class UserDto {
-  name?: string;
-  lastname?: string;
-  age?: number;
-  gender?: string;
-  birthdate?: string;
+  name?: string = ""
+  lastname?: string = ""
+  age?: number = 0
+  gender?: string = ""
+  birthdate?: string = ""
   picture?: Blob;
-  familyQuota?: number;
-  userQuota?: number;// from 0 to 100 or more if overloaded
-  family?: UserDto[]; // user: prenom, photo, dateNaissance, quota, genre, 
-  invoices?: Invoice[];
-  city?: City;
-
-  getChildrenTileText() {
-    var count = 0;
-    for(var fam of this.family!) {
-      if(fam.age! <18) {
-        count++;
-      }
-    }
-    if(count == 0) {
-      return "aucun enfant";
-    } else if(count == 1) {
-      return "1 enfant";
-    } else {
-      return count+" enfants";
-    }
-  }
-
-  isFamilyNumberQuotaReached() {
-    for(var fam of this.family!) {
-      if(fam.userQuota! >=90) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  familyStatus() {
-    if(this.age! > 18) {
-      return "enfant";
-    } else if(this.age! > 64) {
-      return "grand-parent";
-    } else {
-      return "parent";
-    }
-  }
-
-  userQuotaStatus() {
-    if(this.userQuota! > 65) {
-      return "quota--high"
-    } else if(this.userQuota! > 35) {
-      return "quota--medium";
-    } else {
-      return "quota--low";
-    }
-  }
-
-  familyQuotaStatus() {
-    if(this.familyQuota! > 65) {
-      return "quota--high"
-    } else if(this.familyQuota! > 35) {
-      return "quota--medium";
-    } else {
-      return "quota--low";
-    }
-  }
-
-  cityQuotaStatus() {
-    if(this.city?.quota! > 65) {
-      return "quota--high"
-    } else if(this.city?.quota! > 35) {
-      return "quota--medium";
-    } else {
-      return "quota--low";
-    }
-  }
-
-  
-
+  familyQuota?: number = 0
+  personalQuota?: number = 0// from 0 to 100 or more if overloaded
+  familyMembers?: UserDto[] = [] // user: prenom, photo, dateNaissance, quota, genre, 
+  invoices?: Invoice[] = []
+  city?: City = new City;
 }
 
 export class City {
@@ -85,7 +17,9 @@ export class City {
   punishments?: string[];
   punishmentsSoon?: string[];
 
-  constructor(quota: number, punishments: string[],punishmentsSoon: string[]) {
+
+
+  constructor(quota: number = 0, punishments: string[] = [],punishmentsSoon: string[] = []) {
     this.quota = quota;
     this.punishments = punishments;
     this.punishmentsSoon = punishmentsSoon;
@@ -93,31 +27,15 @@ export class City {
 }
 
 export class Invoice {
-  label?: string;
+  productQualifier?: string;
   date?: string;
   productPrice?: number;
   quota?: number;
 
-  constructor(label: string, date: string, productPrice: number, quota: number) {
-    this.label= label;
+  constructor(productQualifier: string, date: string, productPrice: number, quota: number) {
+    this.productQualifier= productQualifier;
     this.date = date;
     this.productPrice = productPrice;
     this.quota = quota;
-  }
-
-  quotaColor() {
-    if(this.quota! < 0) {
-      return "transac__credit  transac--positif";
-    } else {
-      return "transac__credit transac--negatif";
-    }
-  }
-
-  quotaFormatted() {
-    if(this.quota! < 0) {
-      return this.quota;
-    } else {
-      return "+"+this.quota;
-    }
   }
 }
--- src/app/menu/quota-dashbaord/quota-dashboard.service.ts
+++ src/app/menu/quota-dashbaord/quota-dashboard.service.ts
@@ -1,10 +1,11 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { UserDto } from "src/app/domain/user.dto";
 import { environment } from "src/environments/environment";
+import { MainService } from "src/services/main.service";
 
-const apiUrl = environment.API_URL + "api/dashboard/";
+const apiUrl = environment.API_URL + "user/dashboard";
 
 @Injectable({
     providedIn: 'root'
@@ -12,11 +13,17 @@ const apiUrl = environment.API_URL + "api/dashboard/";
 export class QuotaDashboardService {
 
     constructor(
-        private httpClient: HttpClient
+        private httpClient: HttpClient,
+        private mainService: MainService
     ) {}
 
     getData(): Observable<UserDto>{
-        return this.httpClient.get<UserDto>(apiUrl);
+
+        var headers = new HttpHeaders();
+        console.log(this.mainService.getToken().access);
+        headers = headers.set('Token', this.mainService.getToken().access);
+        console.log(headers);
+        return this.httpClient.get<UserDto>(apiUrl,{headers: headers});
     }
 
 }
