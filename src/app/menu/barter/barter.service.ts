3fb5cca60d15f327585c8f33b2775531961dafab
Félix DAUNE <felix.daune@ecole.ensicaen.fr>
2024-01-18 15:19:47 +0100

    API url

--- src/app/menu/barter/barter.service.ts
+++ src/app/menu/barter/barter.service.ts
@@ -2,17 +2,17 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from 'rxjs';
 import { MarketItemDTO } from "./barter.DTO";
+import { environment } from "src/environments/environment.prod";
 
 @Injectable({providedIn: 'root'})
 
 export class BarterService {
-    private apiUrl = '';
+    private apiUrl = environment.API_URL + "api/barter/";
 
     constructor(private httpClient: HttpClient) {}
 
     getMarketItems():Observable<MarketItemDTO[]> {
-        // Il faudra peut-être un paramètre
-        const url = ''; // Faire l'appel API, on verra comment ça marche
+        const url = this.apiUrl + ''; // Faire l'appel API, on verra comment ça marche
         return this.httpClient.get<MarketItemDTO[]>(url);
     }
 
